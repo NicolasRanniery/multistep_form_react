d677ac85efefcef53fb6d840f11c17d60f41511b
NicolasRanniery <nicolasranniery123@gmail.com>
2024-09-18 11:35:43 -0300

    Estilização do Formulário

--- src/App.jsx
+++ src/App.jsx
@@ -25,30 +25,31 @@ function App() {
           avaliar o produto.
         </p>
       </div>
-      <div className="form-container"></div>
-      <p>Etapas</p>
-      <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
-        <div className="inputs-container">{currentComponent}</div>
-        <div className="actions">
-          {!isFirstStep && (
-            <button type="button" onClick={() => changeStep(currentStep - 1)}>
-            <GrFormPrevious />
-            <span>Voltar</span>
-          </button>
-          )}
-          {!isLastStep ? (
-            <button type="submit">
-              <span>Avançar</span>
-              <GrFormNext />
-            </button>
-          ) : (
-            <button type="button">
-              <span>Enviar</span>
-              <FiSend />
-            </button>
-          )}
-        </div>
-      </form>
+      <div className="form-container">
+        <p>Etapas</p>
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
+          <div className="inputs-container">{currentComponent}</div>
+          <div className="actions">
+            {!isFirstStep && (
+              <button type="button" onClick={() => changeStep(currentStep - 1)}>
+                <GrFormPrevious />
+                <span>Voltar</span>
+              </button>
+            )}
+            {!isLastStep ? (
+              <button type="submit">
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+            ) : (
+              <button type="button">
+                <span>Enviar</span>
+                <FiSend />
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
