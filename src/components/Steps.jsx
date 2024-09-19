862eed54da46f5ec2402e91eece8a8faf314066c
NicolasRanniery <nicolasranniery123@gmail.com>
2024-09-19 10:21:56 -0300

    Estilização da etapa "Avaliação" do formulário

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -9,11 +9,11 @@ const Steps = ({ currentStep }) => {
         <AiOutlineUser />
         <p>Identificação</p>
       </div>
-      <div className={`step ${currentStep >= 1 ? "active":""}`}>
+      <div className={`step ${currentStep >= 1 ? "active" : ""}`}>
         <AiOutlineStar />
         <p>Avaliação</p>
       </div>
-      <div className={`step ${currentStep >= 2 ? "active":""}`}>
+      <div className={`step ${currentStep >= 2 ? "active" : ""}`}>
         <FiSend />
         <p>Envio</p>
       </div>
